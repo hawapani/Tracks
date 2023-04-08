bcde88110b9d52eb690507126f26b158ba22076f
Ashutosh Kumar Singh <ashutoshphotoneutron@gmail.com>
2023-04-08 10:49:04 +0530

    Mock location added

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://fa47-2405-201-4011-b91f-e180-c646-30e2-7a8.in.ngrok.io' 
+    baseURL: 'https://3652-2405-201-4011-b91f-692d-54a7-8b6e-b8d5.ngrok-free.app' 
 });
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,10 +1,49 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+//import '../_mockLocation';
+import React, {useState, useEffect} from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Text } from 'react-native-elements';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { requestForegroundPermissionsAsync, watchPositionAsync, Accuracy} from 'expo-location';
+import Map from '../components/Map';
 
 const TrackCreateScreen = () => {
-  return <Text style={{ fontSize: 48 }}>TrackCreateScreen</Text>;
+  const [err, setErr] = useState(null);
+
+  const startWatching = async () => {
+    try {
+      const {granted} = await requestForegroundPermissionsAsync();
+      if (!granted) {
+        throw new Error('Location permission not granted')
+      } else {
+        await watchPositionAsync({
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 10
+        }, (location) => {
+          console.log(location);
+        });
+      }
+    } catch (e) {
+      setErr(e);
+    }
+  }
+
+  useEffect(() => {
+    startWatching();
+  },[]);
+
+  return <SafeAreaView forceInset={{top: 'always'}}>
+    <Text h2>Create a Track</Text>
+    <Map />
+    {err ? <Text>Please allow location permission</Text> : null}
+  </SafeAreaView>
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  dabba: {
+    borderWidth: 5,
+    borderColor: 'red'
+  }
+});
 
 export default TrackCreateScreen;
